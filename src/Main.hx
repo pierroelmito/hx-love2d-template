9ead1ccd2089b01c09c179f9f465c23de1211ef8
pierroelmito <pierroelmito@gmail.com>
2019-11-03 12:32:44 +0100

    format + silly fix

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,3 @@
-
 import love.Love;
 import love.event.EventModule;
 import love.graphics.GraphicsModule;
@@ -8,25 +7,23 @@ import love.image.ImageModule;
 import love.window.WindowModule;
 import love.mouse.MouseModule;
 
-typedef Color = { r: Float, g: Float, b: Float, a: Float }
+typedef Color = {r:Float, g:Float, b:Float, a:Float}
 
-class Game
-{
-	var vis: Bool = false;
-	var x: Float = 10.0;
-	var y: Float = 10.0;
-	var frame: Float = 0;
-	var img1: Image = null;
-	var img2: Image = null;
-	var img3: Image = null;
+class Game {
+	var vis:Bool = false;
+	var x:Float = 10.0;
+	var y:Float = 10.0;
+	var frame:Float = 0;
+	var img1:Image = null;
+	var img2:Image = null;
+	var img3:Image = null;
 
-	public function new() {
-	}
+	public function new() {}
 
-	function genImage(w: Int, h: Int, bilinear: Bool, f: Int -> Int -> Color): Image {
-		var id = ImageModule.newImageData(16, 16);
-		for (y in  0...h) {
-			for (x in  0...w) {
+	function genImage(w:Int, h:Int, bilinear:Bool, f:Int->Int->Color):Image {
+		var id = ImageModule.newImageData(w, h);
+		for (y in 0...h) {
+			for (x in 0...w) {
 				var c = f(x, y);
 				id.setPixel(x, y, c.r, c.g, c.b, c.a);
 			}
@@ -37,21 +34,31 @@ class Game
 		return r;
 	}
 
-	public function load(args: lua.Table<Dynamic,Dynamic>) {
+	public function load(args:lua.Table<Dynamic, Dynamic>) {
 		WindowModule.setTitle("yolo");
 		MouseModule.setVisible(false);
-		img1 = genImage(16, 16, false, function (x, y) {
-			return { r:(x ^ y) & 1, g:1, b:1, a:1 };
+		img1 = genImage(16, 16, false, function(x, y) {
+			return {
+				r: (x ^ y) & 1,
+				g: 1,
+				b: 1,
+				a: 1
+			};
 		});
-		img2 = genImage(16, 16, true, function (x, y) {
+		img2 = genImage(16, 16, true, function(x, y) {
 			var dx = (x - 7.5) / 7.5, dy = (y - 7.5) / 7.5;
 			var v = 1 - Math.min(1, Math.sqrt(dx * dx + dy * dy));
-			return { r:1, g:1, b:1, a:v };
+			return {
+				r: 1,
+				g: 1,
+				b: 1,
+				a: v
+			};
 		});
 		img3 = GraphicsModule.newImage("crosshair.png");
 	}
 
-	public function draw(): Void {
+	public function draw():Void {
 		var mode = WindowModule.getMode();
 		var sz = img1.getDimensions();
 		var cx = 0.5 * mode.width, cy = 0.5 * mode.height;
@@ -59,39 +66,40 @@ class Game
 		GraphicsModule.setColor(1, 1, 1, 1);
 		GraphicsModule.draw(img1, cx, cy, -0.01 * frame, s1, s1, 0.5 * sz.width, 0.5 * sz.height);
 		GraphicsModule.setColor(1, 0, 0, 1);
-		GraphicsModule.draw(img2, cx + r * Math.cos(0.07 * frame), cy + r * Math.sin(0.08 * frame),  0, s2, s2, 0.5 * sz.width, 0.5 * sz.height);
+		GraphicsModule.draw(img2, cx + r * Math.cos(0.07 * frame), cy + r * Math.sin(0.08 * frame), 0, s2, s2, 0.5 * sz.width, 0.5 * sz.height);
 		if (vis) {
 			GraphicsModule.setColor(0.0, 1.0, 0.0, 1.0);
 			GraphicsModule.draw(img3, x - 8, y - 8);
 		}
 	}
 
-	public function update(dt: Float): Void {
+	public function update(dt:Float):Void {
 		frame += dt * 60.0;
 	}
 
-	public function keypressed(k, c: String, b: Bool): Void {
+	public function keypressed(k, c:String, b:Bool):Void {
 		switch (c) {
-			case "escape": EventModule.quit(); 
+			case "escape":
+				EventModule.quit();
 		}
 	}
 
-	public function touchmoved(id: lua.UserData, x, y, dx, dy, pressure: Float): Void {
+	public function touchmoved(id:lua.UserData, x, y, dx, dy, pressure:Float):Void {
 		this.x = x;
 		this.y = y;
 	}
 
-	public function touchpressed(id: lua.UserData, x, y, dx, dy, pressure: Float): Void {
+	public function touchpressed(id:lua.UserData, x, y, dx, dy, pressure:Float):Void {
 		vis = true;
 	}
 
-	public function touchreleased(id: lua.UserData, x, y, dx, dy, pressure: Float): Void {
+	public function touchreleased(id:lua.UserData, x, y, dx, dy, pressure:Float):Void {
 		vis = false;
 	}
 }
 
 class Main {
-	static public function main(): Void {
+	static public function main():Void {
 		var g = new Game();
 		Love.load = g.load;
 		Love.draw = g.draw;
